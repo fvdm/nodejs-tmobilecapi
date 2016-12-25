cb1426dc573a78ba7cbb203bc473c4773bda84d2
Franklin van de Meent <fr@nkl.in>
2016-12-25 20:25:02 +0100

    Refactor(main): Create index.js for intro comment

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+Name:             tmobilecapi
+Description:      T-Mobile NL Customer API module for Node.js
+License:          Unlicense (see LICENSE file)
+Author:           Franklin van de Meent (https://frankl.in)
+Source & docs:    https://github.com/fvdm/nodejs-tmobilecapi
+Feedback & bugs:  https://github.com/fvdm/nodejs-tmobilecapi/issues
+*/
